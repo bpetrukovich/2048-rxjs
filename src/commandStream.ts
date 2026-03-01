d74223053d222fc89ab2fab23a7108ba45df3c41
bpetrukovich <petrukovichbogdan@gmail.com>
2026-03-01 23:20:01 +0300

    add hammerjs

--- src/commandStream.ts
+++ src/commandStream.ts
@@ -1,5 +1,18 @@
 import { filter, fromEvent, map, type Observable } from "rxjs";
 import { Commands, type Command } from "./logic";
+import Hammer from "hammerjs";
+
+const hammer = new Hammer(document.body);
+
+hammer.get("swipe").set({ direction: Hammer.DIRECTION_ALL });
+
+const swipe$ = fromEvent(hammer, "swipe").pipe(
+  map((event: any) => event.direction),
+);
+
+swipe$.subscribe((direction) => {
+  console.log("Swipe direction:", direction);
+});
 
 export const commandStream$: Observable<Command> = fromEvent<KeyboardEvent>(
   document,
